be3d3afef0a2e0b26f1f8ea5a2d07c0901944097
Jitirana <alexandrealvesneto@protonmail.com>
2026-07-10 09:57:40 -0300

    Create install.sh

---
+++ Installers/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "======================================"
+echo " SIDStation Installer v1.0"
+echo "======================================"
+
+sudo bash scripts/install_dependencies.sh
+sudo bash scripts/install_sdrplay.sh
+sudo bash scripts/install_soapysdr.sh
+sudo bash scripts/install_python.sh
+sudo bash scripts/install_influxdb.sh
+sudo bash scripts/install_grafana.sh
+sudo bash scripts/install_systemd.sh
+
+echo
+echo "Installation completed."
+
+echo
+echo "Running verification..."
+
+sudo bash verify.sh
